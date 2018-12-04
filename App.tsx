5d9ce564296788e5585e6dc712bbd49fcd256182
Minami Munakata <june28munazo@gmail.com>
2018-12-04 11:09:07 -0800

    create state

--- App.tsx
+++ App.tsx
@@ -6,6 +6,20 @@ import { Images, Colors, Metrics } from './App/Themes'
 import IDCard from './App/Components/IDCard'
 
 export default class App extends React.Component<{}, {}> {
+  state = {
+    person: {
+      name: 'Unknown',
+      gender: 'Unknown',
+      birthYear: 'N/A',
+      height: 'N/A',
+      weight: 'N/A',
+      hairColor: 'N/A',
+      eyeColor: 'N/A',
+      skinColor: 'N/A',
+    },
+    buttonText: 'Show me your ID Card!',
+  }
+
   constructor(props: {}) {
     super(props)
   }
